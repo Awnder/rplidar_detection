2905ec2519459756e8d4ad6de97cf99e05276a3f
Andrew Shiroma <50969052+Awnder@users.noreply.github.com>
2025-04-07 22:18:12 -0700

    Create buzzer.py
    copied from animation branch

---
+++ buzzer.py
@@ -0,0 +1,40 @@
+# plug in lidar sensor and identify port with ls /dev/tty*
+
+import RPi.GPIO as GPIO
+import threading
+
+# Initialize GPIO for speaker
+SPEAKER_PIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SPEAKER_PIN, GPIO.OUT)
+PWM = GPIO.PWM(SPEAKER_PIN, 3000)  # Set frequency to 3000 Hz
+sound_event = threading.Event()
+
+def emit_sound(frequency: int = 10000) -> None:
+    """Emit sound using GPIO pin for the speaker.
+    Args: 
+        frequency (int): Frequency of the sound in Hz. Minimum is 10 kHz according to 
+        https://www.amazon.com/MEETOOT-Ultrasonic-Speaker-Loudspeaker-Piezoelectric/dp/B09L7LHZML
+    """
+    try:
+        while True:
+            PWM.ChangeFrequency(frequency)  # Set frequency for PWM
+            PWM.start(50)  # Start PWM with 50% duty cycle (volume)
+            sound_event.wait()  # Wait for the event to be set/unset (motion detected/ceased)
+            PWM.stop()
+            sound_event.clear()
+    except Exception as e:
+        print(f"Error in sound thread: {e}")
+    finally:
+        PWM.stop()
+
+if __name__ == "__main__":
+    try:
+        sound_thread = threading.Thread(target=emit_sound, args=(3000,), daemon=True)
+        sound_thread.start()
+        
+    except KeyboardInterrupt:
+        print("Stopping...")
+    finally:
+        sound_event.clear()
+        GPIO.cleanup()
